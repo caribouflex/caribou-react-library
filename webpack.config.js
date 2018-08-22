a5c587e9487397fb1fc98c8d34c71eea60c5463c
Arthur TRESSOL <atressol@distech-controls.com>
2018-08-22 10:50:08 -0400

    update package.json to allow a prepublish to build all.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    mode:'development',
+    mode:'production',
     entry: path.resolve(__dirname, 'src/components/index.js'),
     output: {
         path: path.resolve(__dirname, './dist'),
